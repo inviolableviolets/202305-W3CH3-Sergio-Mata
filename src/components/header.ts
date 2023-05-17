4cf49662d44df44c0579ce344f24464c7d199fef
Sergio Mata <sergiomataherrero@gmail.com>
2023-05-17 18:02:41 +0200

    Add footer and header components

---
+++ src/components/header.ts
@@ -0,0 +1,18 @@
+import { Component } from '../models/component';
+import './header.css';
+
+export class Header extends Component {
+  constructor(selector: string, public title: string) {
+    super(selector);
+    this.template = this.createTemplate();
+    this.render();
+  }
+
+  createTemplate() {
+    return `
+    <header>
+      <h1>${this.title}</h1>
+    </header>
+    `;
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { Footer } from './components/footer';
+import { Header } from './components/header';
+import './style.css';
+
+new Header('.app', 'AWESOME PETS');
+new Footer('.app', 'Awesome Pets © 2023');
